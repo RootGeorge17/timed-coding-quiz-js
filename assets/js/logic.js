b4525411f914a73248d5d5b5939d8b2734deb4d4
RootGeorge <g.sopov@edu.salford.ac.uk>
2024-01-16 16:38:25 +0000

    added timer

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,6 +1,8 @@
 var startButton = document.querySelector('#start');
 var answerButtons = document.querySelector('#choices');
+var timer = document.querySelector('#time');
 var currentQuestion = 0;
+var timeVal = 10;
 
 function startQuiz() {
   document.querySelector('#start-screen').classList.remove('start');
@@ -8,34 +10,42 @@ function startQuiz() {
 
   document.querySelector('#questions').classList.remove('hide');
   document.querySelector('#questions').classList.add('start');
+  startTimer();
   displayQuestion(currentQuestion);
 }
 
 function submitAnswer(event) {
-
   if (event.target.id === quizData[currentQuestion].correctAnswer) {
     console.log("Corrent Answer")
-  }
-  else {
+  } else {
     console.log("Wrong Answer")
   }
+
   if (currentQuestion < 4) {
     currentQuestion++;
     changeCurrentQuestion(currentQuestion);
-  }
-  else {
+  } else {
     endQuiz();
   }
-
 }
 
 function endQuiz() {
-  console.log("End Quiz");
   document.querySelector('#questions').classList.remove('start');
   document.querySelector('#questions').classList.add('hide');
 
   document.querySelector('#end-screen').classList.remove('hide');
   document.querySelector('#end-screen').classList.add('start');
+}
+
+function startTimer() {
+  var timerCountdown = setInterval(function () {
+    timeVal--;
+    timer.textContent = timeVal;
+    if (timeVal <= 0) {
+      clearInterval(timerCountdown);
+      endQuiz();
+    }
+  }, 1000);
 }
 
 startButton.addEventListener('click', startQuiz);
